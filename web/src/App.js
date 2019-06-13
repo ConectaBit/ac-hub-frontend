5415b463d22653359cadd0e722e55e976fd7fa1c
jrquadros <quadrosjunioradits@gmail.com>
2019-06-13 08:26:27 -0300

    storybook added

--- web/src/App.js
+++ web/src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
+import styled from "styled-components";
 import Header from "./components/header";
+
 
 function App() {
   return (
     <>
-      <Header
-        firstItem={{ label: "Explore" }}
-        secondItem={{ label: "Outras Bases" }}
-        thirdItem={{ label: "Login" }}
-        button={{ label: "Cadastre-se" }}
-      />
+      <div>
+        <Header
+          firstItem={{ label: "Explore" }}
+          secondItem={{ label: "Outras Bases" }}
+          thirdItem={{ label: "Login" }}
+          button={{ label: "Cadastre-se" }}
+        />
+      </div>
+     
     </>
   );
 }
