2d96015e232a74a01d64c2b410f75c78b0745413
jrquadros <quadrosjunioradits@gmail.com>
2019-06-28 00:38:14 -0300

    Feature/ timeline component

---
+++ web/src/pages/article/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+import Header from "../../components/header";
+import { Timeline, TimelineItem } from "vertical-timeline-component-for-react";
+
+const Layout = styled.div`
+  width: 100vw;
+`;
+
+function Article() {
+  return (
+    <>
+      <Header />
+      <Layout>
+        <Timeline />
+      </Layout>
+    </>
+  );
+}
+
+export default Article;
--- web/src/routes.js
+++ web/src/routes.js
@@ -1,15 +1,19 @@
-import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import Home from './pages/home'
-import Feed from './pages/feed'
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./pages/home";
+import Feed from "./pages/feed";
+import Article from "./pages/article";
 
 function Routes() {
-    return( <BrowserRouter>
-    <Switch>
-        <Route path='/feed' component={Feed} />
-        <Route path='/' component={Home} />
-    </Switch>
-    </BrowserRouter>)
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/feed" component={Feed} />
+        <Route path="/article" component={Article} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
-export default Routes
+export default Routes;
