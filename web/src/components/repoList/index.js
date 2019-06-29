e677144ac14f81094a90b7f9f4ef20da59c860a5
jrquadros <quadrosjunioradits@gmail.com>
2019-06-28 23:07:10 -0300

    feat(apollo): posts query

--- web/src/components/repoList/index.js
+++ web/src/components/repoList/index.js
@@ -1,24 +1,26 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import { BookContent } from "styled-icons/boxicons-regular/BookContent";
+import {useQuery} from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 import { DominoSpinner } from "react-spinners-kit";
 
 const Box = styled.div`
   @import url("https://fonts.googleapis.com/css?family=Varela+Round&display=swap");
   padding: 1rem;
-  border: 1px solid #ccc;
   border-radius: 5px;
   box-sizing: border-box;
   text-align: center;
   display: flex;
+  background-color: #FFF;
   align-items: center;
   justify-content: center;
   flex-direction: column;
   top: 17.5rem;
   position: fixed;
   margin: auto;
+  width: 12rem;
 
-  span {
+  li {
     font-family: "Varela Round", sans-serif;
     letter-spacing: 2px;
     display: block;
@@ -33,43 +35,30 @@ const Box = styled.div`
   }
 `;
 
-const IconBook = styled(BookContent)`
-  color: inherit;
-  font-size: 1rem;
-
-  :hover {
-    color: inherit;
+const FETCH_REPOS = gql`
+  query{
+  currentUser{
+    id
+    posts{
+    	title
+      id
+    }
   }
+}
 `;
 
-const reposList = [
-  { title: "repo1", link: "/repo1", id: 1 },
-  { title: "repo2", link: "/repo2", id: 2 },
-  { title: "repo3", link: "/repo3", id: 3 },
-  { title: "repo4", link: "/repo4", id: 4 },
-  { title: "repo5", link: "/repo5", id: 5 },
-  { title: "repo6", link: "/repo6", id: 6 },
-  { title: "repo7", link: "/repo6", id: 7 },
-  { title: "repo8", link: "/repo6", id: 8 },
-  { title: "repo9", link: "/repo6", id: 9 },
-  { title: "repo10", link: "/repo6", id: 10 }
-];
-
 function RepoList() {
-  const [repos, setRepos] = useState(reposList);
+  const {data, loading} = useQuery(FETCH_REPOS); 
 
-  return (
+  console.log(data)
+  return(
     <Box>
-      {repos ? (
-        repos.map(repos => (
-          <span key={repos.id}>
-            {repos.title} <IconBook size={25} />
-          </span>
-        ))
-      ) : (
-        <DominoSpinner color="#59D9D9" size={10} sizeUnit="rem" />
-      )}
-    </Box>
+    {loading ? (
+      <DominoSpinner color="#59D9D9" size={10} sizeUnit="rem" />
+      ) : (<ul>
+        {data.currentUser.posts.map((post) => (<li key={post.id}>{post.title}</li>))}
+      </ul>)
+    }</Box>
   );
 }
 
